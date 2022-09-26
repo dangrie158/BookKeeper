f22de5898ee4d9faf5bab02ab65e56c713b6cbf7
Daniel Grießhaber <griesshaber@hdm-stuttgart.de>
2022-09-26 13:15:51 +0200

    added summary for each category of expense

--- bookkeeping/templatetags/entry_stats.py
+++ bookkeeping/templatetags/entry_stats.py
@@ -7,11 +7,22 @@ from django import template
 from bookkeeping.models import BookEntry
 
 
+def filter_category(entries: Iterable[BookEntry], property: str) -> Iterable[BookEntry]:
+    entries = filter(lambda e: getattr(e, property), entries)
+    return entries
+
+
 def entry_sum(entries: Iterable[BookEntry], entry_type: str | None = None):
     if entry_type is not None:
         entries = filter(lambda e: e.type == entry_type, entries)
 
     entry_amounts = map(lambda e: -e.amount if e.type == "EX" else e.amount, entries)
+    return sum(entry_amounts)
+
+
+def property_sum(entries: Iterable[BookEntry], property_name: str):
+    print(entries)
+    entry_amounts = map(lambda e: getattr(e, property_name), entries)
     return sum(entry_amounts)
 
 
@@ -29,5 +40,7 @@ def zip_entries(group: Iterable[BookEntry]):
 
 
 register = template.Library()
+register.filter("filter_category", filter_category)
 register.filter("entry_sum", entry_sum)
+register.filter("property_sum", property_sum)
 register.filter("zip_entries", zip_entries)
